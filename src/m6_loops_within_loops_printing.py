80e43cb2220cf46e226a25fd199e3886f50e73d8
NicholasPSnow <44656056+NicholasPSnow@users.noreply.github.com>
2019-01-23 09:16:49 -0500

    M6

--- src/m6_loops_within_loops_printing.py
+++ src/m6_loops_within_loops_printing.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Nicholas Snow.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -76,7 +76,7 @@ def shape(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     ###########################################################################
@@ -88,8 +88,22 @@ def shape(r):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  15 minutes.
     # -------------------------------------------------------------------------
-
-
+    for i in range(r):
+        #Spaces:
+        for j in range(i):
+            print(' ',end='')
+        #Pluses:
+        for k in range(r-i,0,-1):
+            print('+',end='')
+        #Exclaimation Mark
+        print('!',end='')
+        #Numbers
+        for m in range(r-i,0,-1):
+            print(m,end='')
+        #Dashes:
+        for n in range(i):
+            print('-',end='')
+        print()
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
